4b2e8ca16d74a424dfc08da65d25f310d4ea5833
itsmaddy40 <128139308+itsmaddy40@users.noreply.github.com>
2023-06-03 10:06:51 +0500

    Update BatteryPickUp.cs

--- Inv Scripts/BatteryPickUp.cs
+++ Inv Scripts/BatteryPickUp.cs
@@ -1,3 +1,9 @@
+/*this class represents a battery pickup object in a Unity game. 
+When the player interacts with the battery pickup, it adds a specified number of batteries to the player's inventory and destroys the pickup object. The _batteryPickUpActivated flag ensures that the battery pickup can only be collected once.*/
+
+
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,14 +17,18 @@ public class BatteryPickUp : MonoBehaviour
       _batteryPickUpActivated = false;
     }
 
-    public void PickUpBattery() {
-        if(_batteryPickUpActivated == true)
-        return;
+  public void PickUpBattery() {
+    if (_batteryPickUpActivated == true) // Check if the battery pickup has already been activated
+        return; // If so, exit the method
 
-        Inventory temp = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
-        temp.BatteryToInventory(_addBatteriesToInventory);
+    // Find the player object and get the Inventory component attached to it
+    Inventory temp = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
 
-        Destroy(gameObject);
-        _batteryPickUpActivated = true;
-    }
+    // Add the specified number of batteries to the player's inventory
+    temp.BatteryToInventory(_addBatteriesToInventory);
+
+    Destroy(gameObject); // Destroy the battery pickup object
+    _batteryPickUpActivated = true; // Mark the battery pickup as activated
+}
+
 }
