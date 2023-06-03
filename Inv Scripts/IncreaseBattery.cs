b38d9c54535dffdaf649f581ec6851d866762df3
itsmaddy40 <128139308+itsmaddy40@users.noreply.github.com>
2023-06-03 10:09:46 +0500

    Update IncreaseBattery.cs

--- Inv Scripts/IncreaseBattery.cs
+++ Inv Scripts/IncreaseBattery.cs
@@ -1,3 +1,9 @@
+/*this class represents a battery boost pickup object in a Unity game. 
+When the player interacts with the battery boost pickup, it adds a specified number of battery boosts to the player's inventory and destroys the pickup object. The _batteryBoostPickUpActivated flag ensures that the battery boost pickup can only be collected once.*/
+
+
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,13 +20,17 @@ public class IncreaseBattery : MonoBehaviour
     }
 
    public void PickUpBatteryBoost() {
-    if(_batteryBoostPickUpActivated == true) 
-    return;
+    if (_batteryBoostPickUpActivated == true) // Check if the battery boost pickup has already been activated
+        return; // If so, exit the method
 
+    // Find the player object and get the Inventory component attached to it
     Inventory temp = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+
+    // Add the specified number of battery boosts to the player's inventory
     temp.BatteryBoostToInventory(_addBatteryBoostToInventory);
 
-    Destroy(gameObject);
-    _batteryBoostPickUpActivated = true;
-   }
+    Destroy(gameObject); // Destroy the battery boost pickup object
+    _batteryBoostPickUpActivated = true; // Mark the battery boost pickup as activated
+}
+
 }
